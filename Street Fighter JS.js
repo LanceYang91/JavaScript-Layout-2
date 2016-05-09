a26b23797843ee24b537233e521c4ad1abd68d55
LanceYang91 <SJLVY@outlook.com>
2016-05-09 11:24:51 -0400

    Add files via upload

---
+++ Street Fighter JS.js
@@ -0,0 +1,7 @@
+//change image id2 to image Ken
+function changeImage2(){
+	var image = document.getElementById("img2");
+	image.src = "Ken.png";
+	image.style.width = "400px";
+	image.style.height = "200px";
+}
